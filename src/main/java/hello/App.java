e3708fc79c4bd8e9cd14c215721f38a4d70a5258
rshtishi <rshtishi@3dial.eu>
2020-01-28 11:08:29 +0100

    formatting

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -9,17 +9,19 @@ import al.tirana.pdfBarcodesProcessor.pdfprocessor.PdfDocument;
 public class App {
 
 	public static void main(String[] args) throws Exception {
+
 		ImageProcessor imageProcessor = new OpenCVImageProcessor();
 		imageProcessor.setBarcodeRatioToImage(0.4, 0.6);
 		imageProcessor.setClassifierPath("src/main/resources/classifier-4/cascade.xml");
+
 		PdfBarcodesProcessor pdfBarcodesProcessor = new PdfBarcodesProcessorImpl.Builder()
 				.imageProcessor(imageProcessor).build();
+
 		String filePath = "src/main/resources/test.pdf";
+
 		PdfDocument resultDoc = pdfBarcodesProcessor.processPdfBarcodesPerPage(filePath);
 		resultDoc.getPdfPageList().get(0).getDecodedBarcodeImageMap().keySet().forEach(System.out::println);
 		resultDoc.getPdfPageList().get(1).getDecodedBarcodeImageMap().keySet().forEach(System.out::println);
 		resultDoc.saveAllDecodedBarcodeImages("src/main/resources/decodedBarcodeImages");
-
-
 	}
 }
